97646c8dda7178c0849ad63ac4afa759c3fcab7e
Takayuki Miyagi <m1yag1.takayuk1@gmail.com>
2023-11-23 23:59:11 +0100

    Add files via upload

--- src/TwoBody/M1_2b_current.F90
+++ src/TwoBody/M1_2b_current.F90
@@ -1,11 +1,12 @@
 !
+!
 ! This is just an ad hoc code. Only chiral NLO (LO) two-body current
 ! In the future, I want to replace it with more general one
-! Here some functions are empty. I will update it once I publish a paper.
 !
 module M1_2b_current
   use, intrinsic :: iso_c_binding
-  use MyLibrary, only: pi, hc, m_proton, m_neutron, m_pi, f_pi, g_A
+  use MyLibrary, only: pi, hc, m_proton, m_neutron, m_pi, f_pi, g_A, &
+      & ho_radial_wf, tjs, sjs, snj, dcg, triag, tau1_tau2_tensor_iso, gauss_legendre
   implicit none
   public :: init_M1_2b_current
   public :: fin_M1_2b_current
@@ -19,74 +20,19 @@ module M1_2b_current
   real(8), private, allocatable :: r_mesh(:), w_mesh(:)
   real(8), private, allocatable :: radial_wf_rel(:,:,:)
   real(8), private :: hw
+  real(8), private :: mu_at_rrel = -9999.d0
+  real(8), private :: mu_at_Rcm = -9999.d0
   integer, private :: Nmax, Jmax
   logical, private :: initialized = .false.
 
-  interface
-    function gauss_legendre_allocate(n) &
-          & bind(c,name='gsl_integration_glfixed_table_alloc')
-      import c_int, c_ptr
-      integer(c_int), value, intent(in) :: n
-      type(c_ptr) :: gauss_legendre_allocate
-    end function gauss_legendre_allocate
-    function gauss_legendre_ith_point_weight(a,b,i,xi,wi,t) &
-          & bind(c,name='gsl_integration_glfixed_point')
-      import c_int, c_double, c_ptr
-      real(c_double), value, intent(in) :: a, b
-      integer(c_int), value, intent(in) :: i
-      real(c_double) :: xi, wi
-      type(c_ptr), value, intent(in) :: t
-      integer(c_int) :: gauss_legendre_ith_point_weight
-    end function gauss_legendre_ith_point_weight
-    subroutine gauss_legendre_release(t) &
-          & bind(c,name='gsl_integration_glfixed_table_free')
-      import c_ptr
-      type(c_ptr), value :: t
-    end subroutine gauss_legendre_release
-
-    function coupling_3j(j1,j2,j3,m1,m2,m3) bind(c,name='gsl_sf_coupling_3j')
-      import c_int, c_double
-      real(c_double) :: coupling_3j
-      integer(c_int), value, intent(in) :: j1,j2,j3,m1,m2,m3
-    end function coupling_3j
-
-    ! 6-j symbol
-    function coupling_6j(j1,j2,j3,j4,j5,j6) bind(c,name='gsl_sf_coupling_6j')
-      import c_int, c_double
-      real(c_double) :: coupling_6j
-      integer(c_int), value, intent(in) :: j1,j2,j3,j4,j5,j6
-    end function coupling_6j
-
-    ! 9-j symbol
-    function coupling_9j(j1,j2,j3,j4,j5,j6,j7,j8,j9) bind(c,name='gsl_sf_coupling_9j')
-      import c_int, c_double
-      real(c_double) :: coupling_9j
-      integer(c_int), value, intent(in) :: j1,j2,j3,j4,j5,j6,j7,j8,j9
-    end function coupling_9j
-
-    ! log of Gamma function ln \Gamma(x)
-    function ln_gamma(x) bind(c,name='gsl_sf_lngamma')
-      import c_double
-      real(c_double), value, intent(in) :: x
-      real(c_double) :: ln_gamma
-    end function ln_gamma
-
-    ! associated Laguerre polynomial L^{(a)}_{n}(x)
-    function laguerre(n,a,x) bind(c,name='gsl_sf_laguerre_n')
-      import c_int, c_double
-      integer(c_int), value, intent(in) :: n
-      real(c_double), value, intent(in) :: a, x
-      real(c_double) :: laguerre
-    end function laguerre
-
-  end interface
-
 contains
-  subroutine init_M1_2b_current(hbar_omega, N_max, J_max, N_Mesh, r_max)
+  subroutine init_M1_2b_current(hbar_omega, N_max, J_max, N_Mesh, r_max, op_at_rrel, op_at_Rcm)
     real(8), intent(in) :: hbar_omega
     integer, intent(in) :: N_max, J_max
     integer, intent(in), optional :: N_Mesh
     real(8), intent(in), optional :: r_max
+    real(8), intent(in), optional :: op_at_rrel
+    real(8), intent(in), optional :: op_at_Rcm
     real(8) :: a, v
     integer :: i, l, n
     if(initialized) return
@@ -95,6 +41,8 @@ contains
     Jmax = J_max
     if(present(N_Mesh)) NMesh = N_Mesh
     if(present(r_max)) rmax = r_max
+    if(present(op_at_rrel)) mu_at_rrel = op_at_rrel
+    if(present(op_at_Rcm)) mu_at_Rcm = op_at_Rcm
     call gauss_legendre(0.d0, rmax, r_mesh, w_mesh, NMesh)
     allocate(radial_wf_rel(NMesh,0:Nmax/2,0:Jmax+1))
     radial_wf_rel(:,:,:) = 0.d0
@@ -124,21 +72,52 @@ contains
     logical, intent(in) :: pn
     real(8) :: me
 
-    me = 0.d0
     me = me_M1_2b_current_sachs(nbra, lbra, sbra, jrbra, ncmbra, lcmbra, jbra, zbra, &
         & nket, lket, sket, jrket, ncmket, lcmket, jket, zket, pn)
-    if(ncmbra==ncmket .and. lcmbra==lcmket) then
-      me = me + (-1)**(Lcmket+jrket+Jbra+1) * sjs(2*jrbra, 2*Jbra, 2*Lcmket, 2*Jket, 2*jrket, 2) * &
-          & me_M1_2b_current_intr(nbra, lbra, sbra, jrbra, zbra, nket, lket, sket, jrket, zket, pn)
-    end if
+    if(ncmbra/=ncmket) return
+    if(lcmbra/=lcmket) return
+    me = me + (-1.d0)**(Lcmket+jrket+Jbra+1) * sjs(2*jrbra, 2*Jbra, 2*Lcmket, 2*Jket, 2*jrket, 2) * &
+        & sqrt(dble((2*Jbra+1)*(2*Jket+1))) * &
+        & me_M1_2b_current_intr(nbra, lbra, sbra, jrbra, zbra, nket, lket, sket, jrket, zket, pn)
   end function me_M1_2b_current
 
   function me_M1_2b_current_intr(nbra, lbra, sbra, jbra, zbra, nket, lket, sket, jket, zket, pn) result(me)
     integer, intent(in) :: nbra, lbra, sbra, jbra, zbra
     integer, intent(in) :: nket, lket, sket, jket, zket
     logical, intent(in) :: pn
-    real(8) :: me
+    integer :: k, i
+    real(8) :: a, me, x, prefact, v
+    real(8) :: tau_part, angular_part1, angular_part2
     me = 0.d0
+    if(pn) tau_part = -tau1_tau2_vector_0_pn(lbra, sbra, zbra, lket, sket, zket)
+    if(.not. pn) tau_part = -tau1_tau2_tensor_iso(zbra, zket, 1) * (-sqrt(2.d0))
+    if(abs(tau_part) < 1.d-8) return
+    prefact = -g_A**2 * m_pi / (8.d0 * pi * (2.d0*f_pi)**2)
+    prefact = prefact * (-1.d0) * (2.d0 * m_proton) ! (-1) is from i^2, MeV-1 -> mu_N
+    prefact = prefact * sqrt(3.d0 / (4.d0 * pi))
+
+    angular_part1 = 0.d0 ! [(s1 x s2) . r] r
+    do k = 0, 2, 2
+      if(triag(lbra,lket,k)) cycle
+      angular_part1 = angular_part1 + sqrt(8.d0 * pi / 3.d0) * dcg(2, 0, 2, 0, 2*K, 0) * &
+          & Y_sigma_lsj(lbra, sbra, jbra, lket, sket, jket, K, 1, 1)
+    end do
+    angular_part2 = sqrt(2.d0) * sigma_cross_sigma_lsj(lbra, sbra, jbra, lket, sket, jket, 1) ! s1 x s2 term
+
+    ! r space -> HO
+    if(mu_at_rrel < 0.d0) then
+      do i = 1, NMesh
+      x = m_pi * r_mesh(i) / hc
+      v = prefact * ((1.d0 + 1.d0/x) * angular_part1 + angular_part2) * exp(-x) * tau_part
+      me = me + &
+        & w_mesh(i) * r_mesh(i)**2 * radial_wf_rel(i,nbra,lbra) * v * radial_wf_rel(i,nket,lket)
+      end do
+    else
+      a = 0.25d0 * (m_proton + m_neutron) * hw / hc**2 ! fm-2
+      x = m_pi * mu_at_rrel / hc
+      v = prefact * ((1.d0 + 1.d0/x) * angular_part1 + angular_part2) * exp(-x) * tau_part
+      me = mu_at_rrel**2 * ho_radial_wf(nbra,lbra,a,mu_at_rrel) * ho_radial_wf(nket,lket,a,mu_at_rrel) * v
+    end if
   end function me_M1_2b_current_intr
 
   function me_M1_2b_current_sachs(nbra, lbra, sbra, jrbra, ncmbra, lcmbra, jbra, zbra, &
@@ -146,134 +125,51 @@ contains
     integer, intent(in) :: nbra, lbra, sbra, jrbra, ncmbra, lcmbra, jbra, zbra
     integer, intent(in) :: nket, lket, sket, jrket, ncmket, lcmket, jket, zket
     logical, intent(in) :: pn
-    real(8) :: me
+    integer :: i
+    real(8) :: me, v, a
+    real(8) :: tau_part, b_cm, cm_part, ang_part1, ang_part2, x, prefact
+    integer :: alpha, beta
     me = 0.d0
-  end function me_M1_2b_current_sachs
+    if(pn) tau_part = -tau1_tau2_vector_0_pn(lbra, sbra, zbra, lket, sket, zket)
+    if(.not. pn) tau_part = -tau1_tau2_tensor_iso(zbra, zket, 1) * (-sqrt(2.d0))
+    if(abs(tau_part) < 1.d-8) return
+    b_cm = sqrt( hc**2 / ( (m_proton+m_neutron)*hw ))
+    cm_part = red_r_l(ncmbra, lcmbra, ncmket, lcmket) * b_cm
+    if(abs(cm_part) < 1.d-10) return
 
-  subroutine gauss_legendre(x1,x2,x,w,n)
-    integer, intent(in) :: n
-    real(8), intent(in) :: x1, x2
-    real(8), intent(out), allocatable :: x(:), w(:)
-    real(8) :: xi, wi
-    integer :: info, i
-    type(c_ptr) :: t
-
-    if(allocated(x)) deallocate(x)
-    if(allocated(w)) deallocate(w)
-    allocate(x(n))
-    allocate(w(n))
-    t = gauss_legendre_allocate(n)
-    do i = 1, n
-      info = gauss_legendre_ith_point_weight(x1,x2,i-1,xi,wi,t)
-      x(i) = xi
-      w(i) = wi
+    prefact = g_A**2 * m_pi**2 / (8.d0*pi*(2.d0*f_pi)**2)
+    prefact = prefact * 2.d0 * m_proton / hc * (-1.d0) ! (-1) is from i^2
+    prefact = prefact * sqrt(3.d0 / (4.d0 * pi))
+    ang_part1 = 0.d0
+    do alpha = 0, 2, 2
+      do beta = abs(1-alpha), 1+alpha
+        ang_part1 = ang_part1 + sqrt(8.d0 * pi / 3.d0) * dcg(2, 0, 2, 0, 2*alpha, 0) * dcg(2, 0, 2*alpha, 0, 2*beta, 0) * &
+            & R_Y_sigma_lsLj(ncmbra, lcmbra, lbra, sbra, jrbra, jbra, &
+            & ncmket, lcmket, lket, sket, jrket, jket, beta, alpha, 1, 1) * b_cm
+      end do
     end do
-    call gauss_legendre_release(t)
-  end subroutine gauss_legendre
+    ang_part2 = sqrt(8.d0 * pi) * R_Y_sigma_lsLj(ncmbra, lcmbra, lbra, sbra, jrbra, jbra, &
+        & ncmket, lcmket, lket, sket, jrket, jket, 1, 0, 1, 1) * b_cm
 
-  function dcg(j1, m1, j2, m2, j3, m3) result(s)
     !
-    !  Clebsch-Gordan coefficient
-    !  dcg(j1, m1, j2, m2, j3, m3)
-    !  = ((j1)/2, (m1)/2, (j2)/2, (m2)/2 | (j3)/2, (m3)/2)
-    integer, intent(in) :: j1, j2, j3, m1, m2, m3
-    real(8) :: s
-    s = coupling_3j(j1,j2,j3,m1,m2,-m3) * sqrt(dble(j3+1)) * (-1.d0) ** ((j1-j2+m3)/2)
-  end function dcg
-
-  function tjs(j1, j2, j3, m1, m2, m3) result(r)
-    real(8) :: r
-    integer, intent(in) :: j1, j2, j3, m1, m2, m3
-    r = coupling_3j(j1,j2,j3,m1,m2,m3)
-  end function tjs
-
-  function sjs(j1, j2, j3, l1, l2, l3) result(s)
-    !
-    !  6j coefficient
-    !  d6j(j1, j2, j3, l1, l2, l3) = {(j1)/2 (j2)/2 (j3)/2}
-    !                                {(l1)/2 (l2)/3 (l3)/2}
-    integer, intent(in) :: j1, j2, j3, l1, l2, l3
-    real(8) :: s
-    s = coupling_6j(j1,j2,j3,l1,l2,l3)
-  end function sjs
-
-  function snj(j11, j12, j13, j21, j22, j23, j31, j32, j33) result(s)
-    !
-    !  9j coefficient
-    !  d9j(j11, j12, j13, j21, j22, j23, j31, j32, j33)
-    !    {(j11)/2 (j12)/2 (j13)/2}
-    !  = {(j21)/2 (j22)/2 (j23)/2}
-    !    {(j31)/2 (j32)/2 (j33)/2}
-    integer, intent(in) :: j11, j12, j13, j21, j22, j23, j31, j32, j33
-    real(8) :: s
-    s = coupling_9j(j11,j12,j13,j21,j22,j23,j31,j32,j33)
-  end function snj
-
-  function ho_radial_wf(n,l,anu,r) result(s)
-    ! R = sqrt( 2 * nu**3 * Gamma(n+1) / Gamma(n+l+1.5) ) x^{l} e^{ -x^2/2 } L^{n}_{l+0.5}( x^2 )
-    ! x = r * nu or p * nu
-    ! nu = sqrt(m w / h) or sqrt( h / mw )
-    ! Note that anu = nu^2
-    integer,intent(in) :: n,l
-    real(8),intent(in) :: anu,r
-    real(8) :: s
-    real(8) :: prefact, exp_component, nu, x, a
-    nu = sqrt(anu)
-    x = nu * r
-    a = dble(l)+0.5d0
-    prefact = sqrt( 2.d0 * nu**3 )
-    exp_component = 0.5d0*ln_gamma(dble(n+1)) - 0.5d0*ln_gamma(dble(n+l)+1.5d0) - 0.5d0*x**2 &
-        & + dble(l) * log(x)
-    s = prefact * exp(exp_component) * laguerre(n,a,x**2)
-  end function ho_radial_wf
-
-  function tau1_minus_tau2_pn(lbra,sbra,zbra,lket,sket,zket) result(r)
-    ! tau_{1,z} - tau_{2,z}
-    integer, intent(in) :: lbra, sbra, zbra, lket, sket, zket
-    real(8) :: r
-    r = 0.d0
-    if(zbra /= zket) return
-    if(abs(zket) == 1) return
-    r = -1.d0 + (-1.d0)**(lbra+sbra+lket+sket)
-  end function tau1_minus_tau2_pn
-
-  function tau1_tau2_vector_0_pn(lbra,sbra,zbra,lket,sket,zket) result(r)
-    ! tau_{1,x}tau_{2,y} - tau_{1,y}tau_{2,x}
-    ! i is not taken into account here
-    integer, intent(in) :: lbra, sbra, zbra, lket, sket, zket
-    real(8) :: r
-    r = 0.d0
-    if(zbra /= zket) return
-    if(abs(zket) == 1) return
-    r = (-1.d0)**(lbra+sbra) - (-1.d0)**(lket+sket)
-  end function tau1_tau2_vector_0_pn
-
-  function tau1_tau2_tensor_iso(tbra, tket, Trank) result(r)
-    !  < Tbra || [tau1 x tau2]^{Trank} || Tket >
-    use MyLibrary, only: snj
-    integer, intent(in) :: tbra, tket, Trank
-    real(8) :: r
-    r = 6.d0 * sqrt(dble((2*Tbra+1)*(2*Tket+1)*(2*Trank+1))) * &
-        & snj(1, 1, 2*Tbra, 1, 1, 2*Tket, 2, 2, 2*Trank)
-  end function tau1_tau2_tensor_iso
-
-  function sigma1(Sbra,Sket) result(r)
-    ! < Sbra || sigma_{1} || Sket >
-    integer, intent(in) :: Sbra, Sket
-    real(8) :: r
-    r = sqrt(6.d0) * (-1.d0)**Sket * &
-        & sqrt(dble( (2*Sbra+1) * (2*Sket+1) ) ) * &
-        & sjs(1, 1, 2, 2*Sket, 2*Sbra, 1)
-  end function sigma1
-
-  function sigma2(Sbra,Sket) result(r)
-    ! < Sbra || sigma_{2} || Sket >
-    integer, intent(in) :: Sbra, Sket
-    real(8) :: r
-    r = sqrt(6.d0) * (-1.d0)**Sbra * &
-        & sqrt(dble( (2*Sbra+1) * (2*Sket+1) ) ) * &
-        & sjs(1, 1, 2, 2*Sket, 2*Sbra, 1)
-  end function sigma2
+    ! r space -> HO
+    if(mu_at_rrel<0.d0) then
+      me = 0.d0
+      do i = 1, NMesh
+        x = m_pi * r_mesh(i) / hc
+        v = (1.d0 + 3.d0 / x + 3.d0 / x**2) * ang_part1 + (1.d0 / x + 1.d0 / x**2) * ang_part2
+        v = v * prefact * exp(-x)
+        me = me + r_mesh(i)**2 * w_mesh(i) * radial_wf_rel(i,nbra,lbra) * v * radial_wf_rel(i,nket,lket)
+      end do
+    else
+      a = 0.25d0 * (m_proton + m_neutron) * hw / hc**2 ! fm-2
+      x = m_pi * mu_at_rrel / hc
+      v = (1.d0 + 3.d0 / x + 3.d0 / x**2) * ang_part1 + (1.d0 / x + 1.d0 / x**2) * ang_part2
+      v = v * prefact * exp(-x)
+      me = mu_at_rrel**2 * ho_radial_wf(nbra,lbra,a,mu_at_rrel) * ho_radial_wf(nket,lket,a,mu_at_rrel) * v
+    end if
+    me = me * tau_part
+  end function me_M1_2b_current_sachs
 
   function R_Y_sigma_lsLj(ncmbra, lcmbra, lbra, sbra, jrbra, jbra, &
         & ncmket, lcmket, lket, sket, jrket, jket, rellrank, srank, relrank, rank) result(r)
@@ -338,24 +234,40 @@ contains
         & snj(2*lbra, 2*sbra, 2*jbra, 2*lket, 2*sket, 2*jket, 2*lrank, 2*srank, 2*jrank)
   end function j_to_ls
 
-  pure real(8) function red_r_l(n1, l1, n2, l2) result(rl)
+  real(8) function red_r_l(n1, l1, n2, l2) result(rl)
     integer, intent(in) :: n1, l1, n2, l2
-    if (n1 == n2 .and. l1 == l2-1) then
-      rl = -dsqrt(dble(l2)*(dble(n2 + l2) + 0.5d0))
-    elseif (n1 == n2-1 .and. l1 == l2+1) then
-      rl = -dsqrt(dble(l2 + 1)*dble(n2))
-    elseif (n1 == n2+1 .and. l1 == l2-1) then
-      rl = dsqrt(dble(l2)*(dble(n2  + 1)))
-    elseif (n1 == n2 .and. l1==l2+1) then
-      rl = dsqrt(dble(l2+1)*(dble(n2 +l2)+1.5d0))
+    real(8) :: a
+    if(mu_at_Rcm<0.d0) then
+      if (n1 == n2 .and. l1 == l2-1) then
+        rl = -dsqrt(dble(l2)*(dble(n2 + l2) + 0.5d0))
+      elseif (n1 == n2-1 .and. l1 == l2+1) then
+        rl = -dsqrt(dble(l2 + 1)*dble(n2))
+      elseif (n1 == n2+1 .and. l1 == l2-1) then
+        rl = dsqrt(dble(l2)*(dble(n2  + 1)))
+      elseif (n1 == n2 .and. l1==l2+1) then
+        rl = dsqrt(dble(l2+1)*(dble(n2 +l2)+1.5d0))
+      else
+        rl = 0.d0
+      end if
     else
-      rl = 0.d0
+      if(abs(l1-l2) == 1) then
+        a = (m_proton + m_neutron) * hw / hc**2 ! fm-2
+        rl = (-1.d0)**l1 * sqrt(dble(2*l1+1)*dble(2*l2+1)) * tjs(2*l1, 2, 2*l2, 0, 0, 0) * &
+          & mu_at_Rcm**3 * ho_radial_wf(n1,l1,a,mu_at_Rcm) * ho_radial_wf(n2,l2,a,mu_at_Rcm) * sqrt(a)
+      else
+        rl = 0.d0
+      end if
     end if
   end function red_r_l
 
-  pure logical function triag(i,j,k)
-      implicit none
-      integer,intent(in)::i,j,k
-      triag = ((i-(j+k))*(i-abs(j-k)) > 0)
-  end function triag
+  function tau1_tau2_vector_0_pn(lbra,sbra,zbra,lket,sket,zket) result(r)
+    ! tau_{1,x}tau_{2,y} - tau_{1,y}tau_{2,x}
+    ! i is not taken into account here
+    integer, intent(in) :: lbra, sbra, zbra, lket, sket, zket
+    real(8) :: r
+    r = 0.d0
+    if(zbra /= zket) return
+    if(abs(zket) == 1) return
+    r = (-1.d0)**(lbra+sbra) - (-1.d0)**(lket+sket)
+  end function tau1_tau2_vector_0_pn
 end module M1_2b_current
